169e1b88dc3e28fe4b3daf5b9a39164bd8db33e2
Reubz <reugeorac@gmail.com>
2023-07-20 23:36:59 +0530

    updated routes

--- routes/main.js
+++ routes/main.js
@@ -7,14 +7,26 @@ const path = require('path')
 const config = require("../config.json");
 
 // Databases
-
+const matchesDB = require('../models/schemas/matches');
+const tournamentsDB = require('../models/schemas/tournaments'); 
+const teamsDB = require('../models/schemas/teams'); 
 
 router.get("/", async (req, res) => {
+
+    let  matchesData = await matchesDB.find({ completed: false });
+    matchesData.forEach(async (match) => {
+        match.team1.data = await teamsDB.find({ id: match.team1.id })
+        match.team2.data = await teamsDB.find({ id: match.team2.id })
+    })
+    let tournamentsData = await tournamentsDB.find().sort({ startDate: 1})
+
     res.status(200).render("home/home.ejs", {
-        // matchesData: ,
-        // tournamentsData: ,
+        matchesData: matchesData,
+        tournamentsData: tournamentsData,
         // blogsData: 
     })
+    console.log(matchesData)
+    // console.log(tournamentsData)
 })
 
 
